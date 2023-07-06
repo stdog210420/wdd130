846b3f56404f814c3a16ce4c52c619e5ab93c32c
stdog210420 <hsingyinlin@gmail.com>
2023-07-07 00:13:45 +0800

    added to my homepage

--- ballet/main.js
+++ ballet/main.js
@@ -3,13 +3,17 @@ displayImages();
 function displayImages() {
   let i;
   const images = document.getElementsByClassName("image");
+  // const dots = document.getElementsByClassName("dot");
   for (i = 0; i < images.length; i++) {
     images[i].style.display = "none";
+    // dots[i].style.display = dots[i];
   }
   index++;
   if (index > images.length) {
     index = 1;
   }
   images[index-1].style.display = "block";
+  // dots[Index-1].style.display= "none";
   setTimeout(displayImages, 2000); 
 }
+
--- practice/main.js
+++ practice/main.js
@@ -13,3 +13,21 @@ function displayImages() {
   images[index-1].style.display = "block";
   setTimeout(displayImages, 2000); 
 }
+
+// let slideIndex = 0;
+// displayImages();
+// function displayImages() {
+//   let i;
+//   let slides = document.getElementsByClassName("image"), dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";  
+//     dots[slideIndex-1].className += " active";
+//   setTimeout(displayImages, 2000); // Change image every 2 seconds
+// }
